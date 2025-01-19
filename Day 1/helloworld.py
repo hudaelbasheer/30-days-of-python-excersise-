7922b1c134d79d297f6e0b2d885d5fce127a0394
hudaelbasheer <77302491+hudaelbasheer@users.noreply.github.com>
2025-01-19 02:29:32 -0800

    Added helloworld.py for Day 1 exercise

---
+++ Day 1/helloworld.py
@@ -0,0 +1,43 @@
+# Exercise: Level 2 - Day 1
+# Question 1: Check the Python version you are using
+import sys
+print("Python Version:", sys.version)
+# Question 2: Perform basic arithmetic operations with 3 and 4
+print("Addition:", 3 + 4)
+print("Subtraction:", 3 - 4)
+print("Multiplication:", 3 * 4)
+print("Division:", 3 / 4)
+print("Modulus:", 3 % 4)
+print("Exponential:", 3 ** 4)
+print("Floor Division:", 3 // 4)
+# Question 3: Print strings
+print("Hidayah")
+print("Dayyib")
+print("Nigeria")
+print("I am enjoying 30 days of Python")
+
+# Question 4: Check data types
+print(type(10))  # int
+print(type(9.8))  # float
+print(type(3.14))  # float
+print(type(4 - 4j))  # complex
+print(type(['Asabeneh', 'Python', 'Finland']))  # list
+print(type("Hidayah"))  # str
+print(type("Dayyib"))  # str
+print(type("Nigeria"))  # str
+
+# Question 3: Print strings
+print("Hidayah")
+print("Dayyib")
+print("Nigeria")
+print("I am enjoying 30 days of Python")
+# Question 4: Check data types
+print(type(10))  # int
+print(type(9.8))  # float
+print(type(3.14))  # float
+print(type(4 - 4j))  # complex
+print(type(['Asabeneh', 'Python', 'Finland']))  # list
+print(type("Hidayah"))  # str
+print(type("Dayyib"))  # str
+print(type("Nigeria"))  # str
+
