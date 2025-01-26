c3becefd81a1cd95f5d00dc8796b9b4b73b6b64f
hudaelbasheer <77302491+hudaelbasheer@users.noreply.github.com>
2025-01-26 00:28:03 -0800

    add# Please enter the commit message for your changes. Lines starting

---
+++ Day8/countries_data.py
@@ -0,0 +1,27 @@
+countries_data = [
+    {
+        "name": "Afghanistan",
+        "capital": "Kabul",
+        "languages": ["Pashto", "Uzbek", "Turkmen"],
+        "population": 27657145,
+    },
+    {
+        "name": "Albania",
+        "capital": "Tirana",
+        "languages": ["Albanian"],
+        "population": 2886026,
+    },
+    {
+        "name": "Algeria",
+        "capital": "Algiers",
+        "languages": ["Arabic"],
+        "population": 40400000,
+    },
+    {
+        "name": "Andorra",
+        "capital": "Andorra la Vella",
+        "languages": ["Catalan"],
+        "population": 78014,
+    },
+    # Add more countries as needed...
+]
